20ac20d9a987c7b6df6b7e706214345dd742e4f7
Alec <alecsmith@live.com>
2019-07-23 19:09:24 -0400

    created the cleaner class work done on whitelist

--- Cleaner.py
+++ Cleaner.py
@@ -2,52 +2,58 @@ import praw
 import requests
 import csv
 import configparser
+
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-#automate
-#whitelist by gilded,subreddit,content,score,awards
-#delete
-#submissions
+# TODO: automate
+# TODO: whitelist by gilded,subreddit,content,score,awards
+# TODO: delete
+# TODO: submissions
 
-#connecting to reddit via praw
-reddit = praw.Reddit(client_id=config.get("connection", "client_id"),
-                     client_secret=config.get("connection", "client_secret"),
-                     password=config.get("connection", "password"),
-                     user_agent=config.get("connection", "user_agent"),
-                     username=config.get("connection", "username"))
+class Cleaner:
+    def __init__(self):
+        reddit = praw.Reddit(client_id=config.get("connection", "client_id"),
+                             client_secret=config.get("connection", "client_secret"),
+                             password=config.get("connection", "password"),
+                             user_agent=config.get("connection", "user_agent"),
+                             username=config.get("connection", "username"))
+    @staticmethod
+    def get_comments(username):
+        link = ("https://api.pushshift.io/reddit/search/comment/?author=" + username)
+        r = requests.get(link).json()
+        comments = r['data']
+        return comments
 
-def get_comments(username):
-    link = ("https://api.pushshift.io/reddit/search/comment/?author=" + username)
-    r = requests.get(link).json()
-    comments = r['data']
-    return comments
+    def edit_comments(self, link):
+        comment = self.reddit.comment(id=link)
+        newMessage = "new comment here"
+        comment.edit(newMessage)
 
-def edit_comments(link):
-    comment = reddit.comment(id=link)
-    newMessage = "new comment here"
-    comment.edit(newMessage)
+    def delete_coments(self,link):
+        comment = self.reddit.comment(id=link)
+        comment.delete()
 
-def delete_coments(link):
-    comment = reddit.comment(id=link)
-    comment.delete()
+    @staticmethod
+    def export_comment(comment):
+        values = [comment['author'], comment['subreddit'], comment['body']]
+        comment_data = open('CommentData.csv', 'a')  # open file for writing
+        csvwriter = csv.writer(comment_data)
+        csvwriter.writerow(values)
+        comment_data.close()
 
-def export_comment(comment):
-    values = [comment['author'], comment['subreddit'], comment['body']]
-    comment_data = open('CommentData.csv', 'a') #open file for writing
-    csvwriter = csv.writer(comment_data)
-    csvwriter.writerow(values)
-    comment_data.close()
+    @staticmethod
+    def white_list():
+        # 'created' distinguished, gilded, score subreddit/#subreddit_id gilded #gildings
+        pass
 
-def white_list():
+    def edit_bulk_comments(self, username):
+        comments = self.get_comments(username)
+        length = len(comments)
+        print(length)
+        for data in comments:
+            print(data['id'])
+            self.export_comment(data)
 
 
-def edit_bulk_comments(username):
-    comments = get_comments(username)
-    length = len(comments)
-    print(length)
-    for data in comments:
-        print(data['id'])
-        export_comment(data)
 
-edit_bulk_comments("ypmihc400")
